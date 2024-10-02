048d71eaca79566c86b810cbc0c88cc1de002ed0
theblaze98 <dionijvargas@gmail.com>
2024-10-02 17:38:43 -0400

    styles

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 const score = document.querySelector('#score')
-const audio = document.querySelector('#audio')
-
 
 const STATES = {
 	START: 0,
